7bf92fc48dafcf7e65f43be083b3c40767881cbb
Vinicius Teodoro <vinicius.teodoro@enacom.com.br>
2017-12-06 21:22:26 -0200

    commit final

--- src/app/components/common/topnavbar.controller.js
+++ src/app/components/common/topnavbar.controller.js
@@ -7,15 +7,13 @@ angular.module('tp-final')
   .controller('TopNavController', function ($rootScope, $state) {
     var vm = this;
     vm.showBox = false;
-    $rootScope.filter = 'BM1';
-    vm.selectedValue = 'BM1';
-    vm.notselectedValue = 'BM25';
+    $rootScope.filter = 1;
+    vm.selectedValue = 'Bool And';
+    vm.notselectedValue = ['Bool And', 'Bool Or', 'Vetorial', 'BM25'];
     vm.state = $state.current.name;
-    vm.functBox = function () {
+    vm.functBox = function (index) {
       vm.showBox = false;
-      var aux  = vm.selectedValue;
-      vm.selectedValue = vm.notselectedValue;
-      vm.notselectedValue = aux;
-      $rootScope.filter = vm.selectedValue;
+      vm.selectedValue = vm.notselectedValue[index];
+      $rootScope.filter = index+1;
     }
   });
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,56 +1,34 @@
 'use strict';
 
 angular.module('tp-final')
-  .controller('MainController', function ($rootScope) {
+  .controller('MainController', function ($rootScope, $http) {
     var vm = this;
     vm.artigos = [];
-    vm.bm1 = [
-      {
-        title: 'titleBM1',
-        id:"1",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      },
-      {
-        title: 'title2BM1',
-        id:"2",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      },
-      {
-        title: 'title3BM1',
-        id:"3",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      }
-    ];
-    vm.bm ='BM1';
+    vm.query = '';
+    vm.model = 1;
     vm.artigos = vm.bm1;
     $rootScope.$watch('filter', function () {
-      if($rootScope.filter && (vm.bm !== $rootScope.filter)){
-        vm.bm = $rootScope.filter;
-        if($rootScope.filter === 'BM1'){
-          vm.artigos = vm.bm1;
-        }
-        else{
-          vm.artigos = vm.bm25;
-        }
+      if($rootScope.filter && (vm.model !== $rootScope.filter)){
+        vm.model = $rootScope.filter;
+        vm.query = '';
+        vm.artigos = [];
+        // console.log(vm.model);
       }
 
     });
+    vm.doRequest = function () {
+      var encode = encodeURIComponent(vm.query);
+      var sendModel = vm.model;
+      console.log(sendModel);
+      $http({
+        method: 'GET',
+        url: 'http://localhost:8080/busca?model=' + sendModel + '&query=' + encode
+      }).then(function successCallback(response) {
+         vm.artigos = response;
+      }, function errorCallback(response) {
 
-    vm.bm25 = [
-      {
-        title: 'title',
-        id:"1",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      },
-      {
-        title: 'title2',
-        id:"2",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      },
-      {
-        title: 'title3',
-        id:"3",
-        description:"sokdaok kaosdk asodk asokokd asokd o doaskd okda"
-      }
-    ];
+      });
+    };
+
+
   });
